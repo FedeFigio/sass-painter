7061a4ca92dc85192f9115b34c6835fb40df9a60
Federico <federicofigini90@gmail.com>
2021-04-17 16:49:54 +0200

    wp

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,2 +1,8 @@
 let mix = require('laravel-mix');
-mix.sass('src/scss/app.scss', 'dist/css/');
+
+
+mix.sass('src/scss/app.scss', 'dist/css/')
+    .copy('src/img', 'dist/images')
+    .options({
+        processCssUrls: false,
+    })
